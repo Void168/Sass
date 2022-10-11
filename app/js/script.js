342173fe95534e929befe1f4954d71d1861c212f
LPHung <phuhung16820@gmail.com>
2022-10-11 18:11:25 +0700

    hamburger menu animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
-console.log('test')
+const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+
+btnHamburger.addEventListener('click', function ()
+{
+    console.log(('click hamburger'))
+
+    if(header.classList.contains('open')) {
+        header.classList.remove('open')
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
+    } else {
+        header.classList.add('open')
+        overlay.classList.add('fade-in')
+        overlay.classList.remove('fade-out')
+    }
+})
+
