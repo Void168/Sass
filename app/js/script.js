28ae1434faf2bfb8d253709210558e92b9bf7080
LPHung <phuhung16820@gmail.com>
2022-10-11 20:28:25 +0700

    display hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,23 @@
 const btnHamburger = document.querySelector('#btnHamburger')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
+const fadeElements = document.querySelectorAll('.has-fade')
 
-btnHamburger.addEventListener('click', function ()
-{
-    console.log(('click hamburger'))
+btnHamburger.addEventListener('click', function () {
+  console.log('click hamburger')
 
-    if(header.classList.contains('open')) {
-        header.classList.remove('open')
-        overlay.classList.remove('fade-in')
-        overlay.classList.add('fade-out')
-    } else {
-        header.classList.add('open')
-        overlay.classList.add('fade-in')
-        overlay.classList.remove('fade-out')
-    }
+  if (header.classList.contains('open')) {
+    header.classList.remove('open')
+    fadeElements.forEach(function (element) {
+      element.classList.remove('fade-in')
+      element.classList.add('fade-out')
+    })
+  } else {
+    header.classList.add('open')
+
+    fadeElements.forEach(function (element) {
+      element.classList.add('fade-in')
+      element.classList.remove('fade-out')
+    })
+  }
 })
-
